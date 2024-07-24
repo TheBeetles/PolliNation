f09c3c97476853f7c5af9953200b5a1294a3adcd
Jason Ng <jng632121@gmail.com>
2024-07-24 11:39:33 -0400

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
             },
         ]
     },
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 export default nextConfig;
