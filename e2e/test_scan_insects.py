f85263abd88f1958cecd51d2881f8a50825bdd1d
Jason Ng <jng632121@gmail.com>
2024-08-11 22:41:57 -0400

    added basic tests

--- e2e/test_scan_insects.py
+++ e2e/test_scan_insects.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import Page, expect
 
 
-def test_login(page: Page):
+def test_image_placement(page: Page):
+    '''
+    Change this to what is actually does
+    '''
     page.goto("localhost:3000/")
     page.wait_for_url("**/login")
 
