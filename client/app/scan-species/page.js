08a2db94f1762351381f5ef26e729cb8cdd7f1fc
manig0923 <152234653+manig0923@users.noreply.github.com>
2024-10-22 17:18:31 -0400

    Update page.js started to change UI

--- client/app/scan-species/page.js
+++ client/app/scan-species/page.js
@@ -12,17 +12,21 @@ export default function ScanSpeciesPage() {
   verifyUser();
 
   const handleScanPlant = () => {
-    console.log('Scan an Insect clicked');
-    router.push('/scan-plant')
+    console.log('Scan a Plant clicked');
+    router.push('/scan-plant');
   };
 
   const handleScanInsect = () => {
     console.log('Scan an Insect clicked');
-    router.push('/scan-insect')
+    router.push('/scan-insect');
   };
 
   const handleSavedSpecies = () => {
     router.push('/saved-species');
+  };
+
+  const handleBack = () => {
+    router.push('/previous-page');
   };
 
   const handleLogout = async () => {
@@ -30,90 +34,8 @@ export default function ScanSpeciesPage() {
       method: 'GET',
     });
     if (res.ok) {
-      router.push('/login')
+      router.push('/login');
     } else {
       setError('Something went wrong');
     }
   };
-
-  return (
-    <div style={styles.container}>
-      <button onClick={handleLogout} style={styles.logoutButton}>Logout</button>
-      <div style={styles.scanContainer}>
-        <div onClick={handleScanPlant} style={styles.scanBox}>
-          <Image src={scanIcon} alt="Scan a Plant" width={100} height={100} />
-          <p style={styles.scanText}>Scan a Plant</p>
-        </div>
-        <p style={styles.orText}>OR</p>
-        <div onClick={handleScanInsect} style={styles.scanBox}>
-          <Image src={scanIcon} alt="Scan an Insect" width={100} height={100} />
-          <p style={styles.scanText}>Scan an Insect</p>
-        </div>
-      </div>
-      <button onClick={handleSavedSpecies} style={styles.savedSpeciesButton}>Saved Species</button>
-      <h1 style={{ color: 'red' }}> { error } </h1>
-    </div>
-  );
-}
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '80vh',
-    backgroundColor: '#fff',
-    padding: '20px',
-    fontfamily: 'Inter',
-  },
-  logoutButton: {
-    alignSelf: 'flex-end',
-    marginBottom: '20px',
-    padding: '10px 20px',
-    fontSize: '16px',
-    borderRadius: '20px',
-    backgroundColor: '#B3E576',
-    color: '#fff',
-    border: 'none',
-    cursor: 'pointer',
-  },
-  scanContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '40vh',
-    margin: '1em 0',
-  },
-  scanBox: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '150px',
-    height: '150px',
-    border: '2px solid #000',
-    borderRadius: '10px',
-    margin: '0 20px',
-    cursor: 'pointer',
-  },
-  scanText: {
-    marginTop: '10px',
-    fontSize: '18px',
-    color: '#2E8B57',
-  },
-  orText: {
-    fontSize: '24px',
-    color: '#000',
-  },
-  savedSpeciesButton: {
-    padding: '10px 20px',
-    fontSize: '16px',
-    borderRadius: '20px',
-    backgroundColor: '#B3E576',
-    color: '#fff',
-    border: 'none',
-    cursor: 'pointer',
-    margin: '1em 0 0 0',
-  },
-};
